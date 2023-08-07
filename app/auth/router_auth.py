9cdc3ce433d8ee9c8e755b8222e93eb1a7299f4f
dinmukhammed <arystambekdimash005@gmail.com>
2023-08-07 14:05:59 +0600

    done

--- app/auth/router_auth.py
+++ app/auth/router_auth.py
@@ -20,12 +20,12 @@ async def registration(user: UserRequest, db: Session = Depends(get_db)):
 
 
 @router.post("/login")
-async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+async def login(form_data:Annotated[OAuth2PasswordRequestForm,Depends()], db: Session = Depends(get_db)):
     db_user = user_repo.get_user_by_username(db, form_data.username)
     if not db_user:
-        raise HTTPException(status_code=404, detail="The user not found")
+        raise HTTPException(status_code=404,detail="The user not found")
     if db_user.password != form_data.password:
-        raise HTTPException(status_code=401, detail="Unauthorized")
+        raise HTTPException(status_code=401,detail="Unauthorized")
     token = encode_jwt(db_user.id)
-    return {"access_token": token}
+    return {"access_token":token,"token_type":"bearer"}
 
